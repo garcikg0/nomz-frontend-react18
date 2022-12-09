692f43cbdd208fce33b236b7813bc4ff34e92687
garcikg0 <kg.gar91@gmail.com>
2022-12-09 17:33:24 -0500

    wrapped routes around App.js with respective paths

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import { Route, Routes, Link } from "react-router-dom"
 import './App.css';
+import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Login";
+import Logout from "./pages/Logout";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return( 
+  <>
+  <nav>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+      <li>
+        <Link to="/logout">Logout</Link>
+      </li>
+    </ul>
+  </nav>
+  <Routes>
+    <Route path="/" element={<LandingPage/>} />
+    <Route path="/login" element={<Login/>} />
+    <Route path="/logout" element={<Logout/>} />
+  </Routes>
+  </>
+  )
 }
 
 export default App;
