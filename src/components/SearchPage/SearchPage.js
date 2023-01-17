37654df394f1bb8ecb5fc87218b716abc0b53db6
garcikg0 <kg.gar91@gmail.com>
2023-01-17 15:46:55 -0500

    SearchPage markup and basic styling complete. Created SearchResultCard component

--- src/components/SearchPage/SearchPage.js
+++ src/components/SearchPage/SearchPage.js
@@ -1,10 +1,38 @@
 import React from "react";
 import './styles.css';
+import SearchResultCard from "../SearchResultCard/SearchResultCard";
 
 const SearchPage = () => {
 
     return(
-        <h1>SearchPage</h1>
+        <>
+        <div id="layout-grid-search-page">
+            <div id="kitchen-nav-container">
+                <nav id="kitchen-navbar">
+                    <ul>
+                        <li>Kitchen Nav</li>
+                    </ul>
+                </nav>
+            </div>
+            <div id="searchbar-container">
+                <form id="search-bar-form" >
+                    <input id="search-bar-input"
+                    type="search"
+                    placeholder="Search..."
+                    autoFocus required
+                    />
+                    <button id="search-bar-button">Go</button>
+                </form>
+            </div>
+            <div id="main-search-page">
+                <div id="search-result-container">
+                    <SearchResultCard />
+                </div>
+            </div>
+        </div>
+      
+        </>
+        
     )
 };
 
---
+++ src/components/SearchResultCard/SearchResultCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './styles.css';
+
+const SearchResultCard = () => {
+
+    return(
+        <p>Search Result Card</p>
+    )
+}
+
+export default SearchResultCard;
