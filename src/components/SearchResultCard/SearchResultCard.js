ee66fc9297c3c7c4f30ff68c2c033854f395baf9
garcikg0 <kg.gar91@gmail.com>
2023-01-25 16:46:51 -0500

    Ingredients button in RecipeCard and SearchResultCard functional

--- src/components/SearchResultCard/SearchResultCard.js
+++ src/components/SearchResultCard/SearchResultCard.js
@@ -1,23 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import './styles.css';
 import chefImage from './chefImage.png'
 
 const SearchResultCard = ( {result}) => {
 
+    const [showIngred, setShowIngred] = useState(false);
+    const activeStatus = showIngred ? '-active' : '';
+
+    const handleIngredClick = e => {
+        e.preventDefault();
+        setShowIngred(!showIngred)
+    }
+
     return(
         <>
         <div id='card'>
             <h1>{result.title}</h1>
-            <img src={chefImage}></img>
+            <img src={chefImage} alt='foo'></img>
             <h2>{result.source}</h2>
             <div id="status">
                 <i className="fas fa-check-circle">
                     <h3>Available</h3>
                 </i>
             </div>
-            <button id='ingred-btn'>Ingredients</button>
+            <button id='ingred-btn' onClick={handleIngredClick}>Ingredients</button>
             <button id='instruct-btn'>Instructions and More Info</button>
             <button id='save-btn'>Save</button>
+        </div>
+        <div id={`ingred-content${activeStatus}`}>
+            <div className="recipe-ingred-title">
+                <h1>Recipe Ingredient</h1>
+            </div>
+            <div className="kitchen-ingred-title">
+                <h1>Kitchen Ingredient</h1>
+            </div>
+            <div className="ingred-actions-title">
+                <h1>Actions</h1>
+            </div>
+            <div className="recipe-ingred">
+                <p>Ingred</p>
+            </div>
+            <div className="kitchen-ingred">
+                <p>Match Confirmed</p>
+            </div>
+            <div className="ingred-actions">
+                <p>Actions</p>
+            </div>
+            <div className="recipe-ingred">
+                <p>Ingred</p>
+            </div>
+            <div className="kitchen-ingred">
+                <p>No Match</p>
+            </div>
+            <div className="ingred-actions">
+                <p>Actions</p>
+            </div>
+            <div className="recipe-ingred">
+                <p>Ingred</p>
+            </div>
+            <div className="kitchen-ingred">
+                <p>Match 1</p>
+                <p>Match 2</p>
+                <p>Match 3</p>
+            </div>
+            <div className="ingred-actions">
+                <p>Actions</p>
+                <p>Actions</p>
+                <p>Actions</p>
+            </div>
         </div>
         </>
     )
